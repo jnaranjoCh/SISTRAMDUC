003031792702d82fa91dc8b0bbbb83820ff04c53
Oriana Ruiz <oriru92@gmail.com>
2017-02-21 17:19:10 -0400

    Commit 1 by @cricri92
    - Creado Bundle de Reincorporación.
    
    - Vistas inicializadas:
    
    -- Inicio.
    -- Actualizar currículum.
    -- Subida de recaudos.
    -- Verificación de datos y Envío de solicitud.
    
    - Ítems agregados en menú de Navegación Principal:
    
    -- Inicio.
    -- Actualizar currículum.
    -- Subida de recaudos.
    -- Envío de solicitud.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new PlanSeptenalBundle\PlanSeptenalBundle(),
             new RegistroUnicoBundle\RegistroUnicoBundle(),
+            new ReincorporacionBundle\ReincorporacionBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
