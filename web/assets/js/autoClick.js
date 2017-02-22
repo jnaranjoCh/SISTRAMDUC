90ad747b48fce59956816b9b18b759a58b0268a4
eulises contreras <eulisescontrerasrengifo1@gmail.com>
2017-02-22 05:10:19 +0000

    vista: registro de datos -> datos de ivestigacion

--- web/assets/js/autoClick.js
+++ web/assets/js/autoClick.js
@@ -1,4 +1,5 @@
 window.onload = function(){
     document.getElementById('mini-1').click();
     document.getElementById('mini-2').click();
+    document.getElementById('mini-3').click();
 };
---
+++ web/assets/js/configTable.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('#table-1').DataTable( {
+	    "language": {
+            	"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+        }
+    });
+    $('#table-2').DataTable( {
+	    "language": {
+            	"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+        }
+    });
+});
