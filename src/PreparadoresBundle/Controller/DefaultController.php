32a09f79d9334064042b6ea591260b308d5b440b
saritagl <sara.anay@gmail.com>
2017-02-24 19:29:25 +0000

    vistas modulo preparadores

--- src/PreparadoresBundle/Controller/DefaultController.php
+++ src/PreparadoresBundle/Controller/DefaultController.php
@@ -3,11 +3,55 @@
 namespace PreparadoresBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    /**
+     * @Route("/preparadores/apertura_concurso", name="apertura_concurso_index")
+     */
+    public function aperturaDeConcursoAction()
     {
-        return $this->render('PreparadoresBundle:Default:index.html.twig');
+        return $this->render('PreparadoresBundle::apertura_concurso.html.twig');
+    }
+    
+    /**
+     * @Route("/preparadores/apertura_concurso/solicitar", name="solicitar")
+     */
+    public function solicitarAction()
+    {
+        return $this->render('PreparadoresBundle::solicitar_concurso.html.twig');
+    }
+    
+    /**
+     * @Route("/preparadores/apertura_concurso/gestionar", name="gestionar_apertura")
+     */
+    public function gestionarAperturaAction()
+    {
+        return $this->render('PreparadoresBundle::gestionar_apertura.html.twig');
+    }
+    
+    /**
+     * @Route("/preparadores/concurso", name="concurso_index")
+     */
+    public function concursoAction()
+    {
+        return $this->render('PreparadoresBundle::concurso.html.twig');
+    }
+    
+    /**
+     * @Route("/preparadores/concurso/gestionar", name="gestionar_concurso")
+     */
+    public function gestionarConcursoAction()
+    {
+        return $this->render('PreparadoresBundle::gestionar_concurso.html.twig');
+    }
+    
+    /**
+     * @Route("/preparadores/renuncia_preparador", name="renuncia_preparador_index")
+     */
+    public function renunciaPreparadorAction()
+    {
+        return $this->render('PreparadoresBundle::renuncia_preparador.html.twig');
     }
 }
