647184514263424e333c3f7b567cf457fa30d8e1
Anyelys Escalona <anyelys15escalona@gmail.com>
2017-03-03 23:22:06 -0400

    Generado TramiteBundle

---
+++ src/TramiteBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TramiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TramiteBundle:Default:index.html.twig');
+    }
+}
