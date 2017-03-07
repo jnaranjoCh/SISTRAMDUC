29fa9ab3f69ec103cb1ea5486380010ab7377d20
saritagl <sara.anay@gmail.com>
2017-03-07 21:44:44 +0000

    Creando Entidades Comunes de Concursos

---
+++ src/ConcursosBundle/Entity/Aspirante.php
@@ -0,0 +1,500 @@
+<?php
+
+namespace ConcursosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Aspirante
+ *
+ * @ORM\Table(name="aspirante")
+ * @ORM\Entity(repositoryClass="ConcursosBundle\Repository\AspiranteRepository")
+ */
+class Aspirante
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="primerNombre", type="string", length=50)
+     */
+    private $primerNombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="segundoNombre", type="string", length=50)
+     */
+    private $segundoNombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="primerApellido", type="string", length=50)
+     */
+    private $primerApellido;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="segundoApellido", type="string", length=50)
+     */
+    private $segundoApellido;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="telefono", type="integer")
+     */
+    private $telefono;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="correo", type="string", length=50)
+     */
+    private $correo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cedula", type="string", length=25)
+     */
+    private $cedula;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="telefonoSecundario", type="integer", nullable=true)
+     */
+    private $telefonoSecundario;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="universidadEgresado", type="string", length=100, nullable=true)
+     */
+    private $universidadEgresado;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcionTituloUniv", type="string", length=100, nullable=true)
+     */
+    private $descripcionTituloUniv;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="anyoGraduacion", type="integer", nullable=true)
+     */
+    private $anyoGraduacion;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="promedioAcademico", type="integer", nullable=true)
+     */
+    private $promedioAcademico;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="notaIntento1", type="integer", nullable=true)
+     */
+    private $notaIntento1;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="notaIntento2", type="integer", nullable=true)
+     */
+    private $notaIntento2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="observaciones", type="string", length=255, nullable=true)
+     */
+    private $observaciones;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set primerNombre
+     *
+     * @param string $primerNombre
+     *
+     * @return Aspirante
+     */
+    public function setPrimerNombre($primerNombre)
+    {
+        $this->primerNombre = $primerNombre;
+
+        return $this;
+    }
+
+    /**
+     * Get primerNombre
+     *
+     * @return string
+     */
+    public function getPrimerNombre()
+    {
+        return $this->primerNombre;
+    }
+
+    /**
+     * Set segundoNombre
+     *
+     * @param string $segundoNombre
+     *
+     * @return Aspirante
+     */
+    public function setSegundoNombre($segundoNombre)
+    {
+        $this->segundoNombre = $segundoNombre;
+
+        return $this;
+    }
+
+    /**
+     * Get segundoNombre
+     *
+     * @return string
+     */
+    public function getSegundoNombre()
+    {
+        return $this->segundoNombre;
+    }
+
+    /**
+     * Set primerApellido
+     *
+     * @param string $primerApellido
+     *
+     * @return Aspirante
+     */
+    public function setPrimerApellido($primerApellido)
+    {
+        $this->primerApellido = $primerApellido;
+
+        return $this;
+    }
+
+    /**
+     * Get primerApellido
+     *
+     * @return string
+     */
+    public function getPrimerApellido()
+    {
+        return $this->primerApellido;
+    }
+
+    /**
+     * Set segundoApellido
+     *
+     * @param string $segundoApellido
+     *
+     * @return Aspirante
+     */
+    public function setSegundoApellido($segundoApellido)
+    {
+        $this->segundoApellido = $segundoApellido;
+
+        return $this;
+    }
+
+    /**
+     * Get segundoApellido
+     *
+     * @return string
+     */
+    public function getSegundoApellido()
+    {
+        return $this->segundoApellido;
+    }
+
+    /**
+     * Set telefono
+     *
+     * @param integer $telefono
+     *
+     * @return Aspirante
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    /**
+     * Get telefono
+     *
+     * @return int
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Set correo
+     *
+     * @param string $correo
+     *
+     * @return Aspirante
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    /**
+     * Get correo
+     *
+     * @return string
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * Set cedula
+     *
+     * @param string $cedula
+     *
+     * @return Aspirante
+     */
+    public function setCedula($cedula)
+    {
+        $this->cedula = $cedula;
+
+        return $this;
+    }
+
+    /**
+     * Get cedula
+     *
+     * @return string
+     */
+    public function getCedula()
+    {
+        return $this->cedula;
+    }
+
+    /**
+     * Set telefonoSecundario
+     *
+     * @param integer $telefonoSecundario
+     *
+     * @return Aspirante
+     */
+    public function setTelefonoSecundario($telefonoSecundario)
+    {
+        $this->telefonoSecundario = $telefonoSecundario;
+
+        return $this;
+    }
+
+    /**
+     * Get telefonoSecundario
+     *
+     * @return int
+     */
+    public function getTelefonoSecundario()
+    {
+        return $this->telefonoSecundario;
+    }
+
+    /**
+     * Set universidadEgresado
+     *
+     * @param string $universidadEgresado
+     *
+     * @return Aspirante
+     */
+    public function setUniversidadEgresado($universidadEgresado)
+    {
+        $this->universidadEgresado = $universidadEgresado;
+
+        return $this;
+    }
+
+    /**
+     * Get universidadEgresado
+     *
+     * @return string
+     */
+    public function getUniversidadEgresado()
+    {
+        return $this->universidadEgresado;
+    }
+
+    /**
+     * Set descripcionTituloUniv
+     *
+     * @param string $descripcionTituloUniv
+     *
+     * @return Aspirante
+     */
+    public function setDescripcionTituloUniv($descripcionTituloUniv)
+    {
+        $this->descripcionTituloUniv = $descripcionTituloUniv;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcionTituloUniv
+     *
+     * @return string
+     */
+    public function getDescripcionTituloUniv()
+    {
+        return $this->descripcionTituloUniv;
+    }
+
+    /**
+     * Set anyoGraduacion
+     *
+     * @param integer $anyoGraduacion
+     *
+     * @return Aspirante
+     */
+    public function setAnyoGraduacion($anyoGraduacion)
+    {
+        $this->anyoGraduacion = $anyoGraduacion;
+
+        return $this;
+    }
+
+    /**
+     * Get anyoGraduacion
+     *
+     * @return int
+     */
+    public function getAnyoGraduacion()
+    {
+        return $this->anyoGraduacion;
+    }
+
+    /**
+     * Set promedioAcademico
+     *
+     * @param integer $promedioAcademico
+     *
+     * @return Aspirante
+     */
+    public function setPromedioAcademico($promedioAcademico)
+    {
+        $this->promedioAcademico = $promedioAcademico;
+
+        return $this;
+    }
+
+    /**
+     * Get promedioAcademico
+     *
+     * @return int
+     */
+    public function getPromedioAcademico()
+    {
+        return $this->promedioAcademico;
+    }
+
+    /**
+     * Set notaIntento1
+     *
+     * @param integer $notaIntento1
+     *
+     * @return Aspirante
+     */
+    public function setNotaIntento1($notaIntento1)
+    {
+        $this->notaIntento1 = $notaIntento1;
+
+        return $this;
+    }
+
+    /**
+     * Get notaIntento1
+     *
+     * @return int
+     */
+    public function getNotaIntento1()
+    {
+        return $this->notaIntento1;
+    }
+
+    /**
+     * Set notaIntento2
+     *
+     * @param integer $notaIntento2
+     *
+     * @return Aspirante
+     */
+    public function setNotaIntento2($notaIntento2)
+    {
+        $this->notaIntento2 = $notaIntento2;
+
+        return $this;
+    }
+
+    /**
+     * Get notaIntento2
+     *
+     * @return int
+     */
+    public function getNotaIntento2()
+    {
+        return $this->notaIntento2;
+    }
+
+    /**
+     * Set observaciones
+     *
+     * @param string $observaciones
+     *
+     * @return Aspirante
+     */
+    public function setObservaciones($observaciones)
+    {
+        $this->observaciones = $observaciones;
+
+        return $this;
+    }
+
+    /**
+     * Get observaciones
+     *
+     * @return string
+     */
+    public function getObservaciones()
+    {
+        return $this->observaciones;
+    }
+}
+
---
+++ src/ConcursosBundle/Entity/Concurso.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace ConcursosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Concurso
+ *
+ * @ORM\Table(name="concurso")
+ * @ORM\Entity(repositoryClass="ConcursosBundle\Repository\ConcursoRepository")
+ */
+class Concurso
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fechaInicio", type="datetime", nullable=true)
+     */
+    private $fechaInicio;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nroVacantes", type="integer")
+     */
+    private $nroVacantes;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="areaPostulacion", type="string", length=100)
+     */
+    private $areaPostulacion;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idUsuario", type="integer")
+     */
+    private $idUsuario;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fechaRecepDoc", type="datetime", nullable=true)
+     */
+    private $fechaRecepDoc;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fechaPresentacion", type="datetime", nullable=true)
+     */
+    private $fechaPresentacion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="observaciones", type="string", length=255, nullable=true)
+     */
+    private $observaciones;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fechaInicio
+     *
+     * @param \DateTime $fechaInicio
+     *
+     * @return Concurso
+     */
+    public function setFechaInicio($fechaInicio)
+    {
+        $this->fechaInicio = $fechaInicio;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaInicio
+     *
+     * @return \DateTime
+     */
+    public function getFechaInicio()
+    {
+        return $this->fechaInicio;
+    }
+
+    /**
+     * Set nroVacantes
+     *
+     * @param integer $nroVacantes
+     *
+     * @return Concurso
+     */
+    public function setNroVacantes($nroVacantes)
+    {
+        $this->nroVacantes = $nroVacantes;
+
+        return $this;
+    }
+
+    /**
+     * Get nroVacantes
+     *
+     * @return int
+     */
+    public function getNroVacantes()
+    {
+        return $this->nroVacantes;
+    }
+
+    /**
+     * Set areaPostulacion
+     *
+     * @param string $areaPostulacion
+     *
+     * @return Concurso
+     */
+    public function setAreaPostulacion($areaPostulacion)
+    {
+        $this->areaPostulacion = $areaPostulacion;
+
+        return $this;
+    }
+
+    /**
+     * Get areaPostulacion
+     *
+     * @return string
+     */
+    public function getAreaPostulacion()
+    {
+        return $this->areaPostulacion;
+    }
+
+    /**
+     * Set idUsuario
+     *
+     * @param integer $idUsuario
+     *
+     * @return Concurso
+     */
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get idUsuario
+     *
+     * @return int
+     */
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * Set fechaRecepDoc
+     *
+     * @param \DateTime $fechaRecepDoc
+     *
+     * @return Concurso
+     */
+    public function setFechaRecepDoc($fechaRecepDoc)
+    {
+        $this->fechaRecepDoc = $fechaRecepDoc;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaRecepDoc
+     *
+     * @return \DateTime
+     */
+    public function getFechaRecepDoc()
+    {
+        return $this->fechaRecepDoc;
+    }
+
+    /**
+     * Set fechaPresentacion
+     *
+     * @param \DateTime $fechaPresentacion
+     *
+     * @return Concurso
+     */
+    public function setFechaPresentacion($fechaPresentacion)
+    {
+        $this->fechaPresentacion = $fechaPresentacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaPresentacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaPresentacion()
+    {
+        return $this->fechaPresentacion;
+    }
+
+    /**
+     * Set observaciones
+     *
+     * @param string $observaciones
+     *
+     * @return Concurso
+     */
+    public function setObservaciones($observaciones)
+    {
+        $this->observaciones = $observaciones;
+
+        return $this;
+    }
+
+    /**
+     * Get observaciones
+     *
+     * @return string
+     */
+    public function getObservaciones()
+    {
+        return $this->observaciones;
+    }
+}
+
