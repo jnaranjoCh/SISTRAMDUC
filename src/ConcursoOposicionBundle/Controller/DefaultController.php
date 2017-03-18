5960b88cb2be85cf59341c5418fae936ba33e026
jnaranjoCh <jnaranjo.facyt.uc@gmail.com>
2017-03-18 09:51:18 -0400

    segunda parte concurso de oposicion

--- src/ConcursoOposicionBundle/Controller/DefaultController.php
+++ src/ConcursoOposicionBundle/Controller/DefaultController.php
@@ -14,4 +14,36 @@ class DefaultController extends Controller
     {
         return $this->render('ConcursoOposicionBundle::apertura_concurso.html.twig');
     }
+
+    /**
+     * @Route("/concursoOposicion/jurado", name="jurado")
+     */
+    public function juradoAction()
+    {
+        return $this->render('ConcursoOposicionBundle::jurado.html.twig');
+    }
+
+    /**
+     * @Route("/concursoOposicion/cpec", name="cpec")
+     */
+    public function cpecAction()
+    {
+        return $this->render('ConcursoOposicionBundle::cpec.html.twig');
+    }
+
+    /**
+     * @Route("/concursoOposicion/tablaBasicaConcurso", name="tablaBasicaConcurso")
+     */
+    public function concursoAction()
+    {
+        return $this->render('ConcursoOposicionBundle::tablaBasica_concurso.html.twig');
+    }
+
+    /**
+     * @Route("/concursoOposicion/registro_usuario_oposicion", name="registro_usuario_oposicion")
+     */
+    public function registroAction()
+    {
+        return $this->render('ConcursoOposicionBundle::registroAspirante.html.twig');
+    }
 }
